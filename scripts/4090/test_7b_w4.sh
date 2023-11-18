1b117d31ce09778b194a2965d072afb3231fa6d0
liujingcs <liujing_95@outlook.com>
2023-11-18 23:17:30 +1100

    Update.

--- scripts/4090/test_7b_w4.sh
+++ scripts/4090/test_7b_w4.sh
@@ -2,5 +2,5 @@ for n in 1 2 3 4 5
 do
 SAVE_PATH=output/llama-7b/w4only
 mkdir -p ${SAVE_PATH}
-python prefill_speed.py --model_name_or_path /data/dongzhiwei/models/llama2-13b 2>&1 | tee -a ${SAVE_PATH}/train_r${r}_n${n}.log
+python examples/benchmark/prefill_speed.py --model_name_or_path /data/dongzhiwei/models/llama2-13b 2>&1 | tee -a ${SAVE_PATH}/train_r${r}_n${n}.log
 done
